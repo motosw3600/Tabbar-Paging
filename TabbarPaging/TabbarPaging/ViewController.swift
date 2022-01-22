f6549ff1ce23ba4eecc9752da3307f6845f59472
motosw3600 <motosw3600@gmail.com>
2022-01-22 15:25:21 +0900

    initial project

---
+++ TabbarPaging/TabbarPaging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbarPaging
+//
+//  Created by 박상우 on 2022/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
