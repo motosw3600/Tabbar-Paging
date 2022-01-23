8316a855bb71ed64d622cb29b82218c97f5a9eb2
motosw3600 <motosw3600@gmail.com>
2022-01-23 15:24:20 +0900

    feat: MenuCollectionViewCell Add

---
+++ TabbarPaging/TabbarPaging/MenuCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCollectionViewCell.swift
+//  TabbarPaging
+//
+//  Created by 박상우 on 2022/01/23.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
