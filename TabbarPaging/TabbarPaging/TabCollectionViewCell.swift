18dd6f6ebb09e6153abc3d5f7ba303f89e65a350
motosw3600 <motosw3600@gmail.com>
2022-01-23 15:23:11 +0900

    feat: TabCollectionViewCell Add

---
+++ TabbarPaging/TabbarPaging/TabCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TabCollectionViewCell.swift
+//  TabbarPaging
+//
+//  Created by 박상우 on 2022/01/23.
+//
+
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
